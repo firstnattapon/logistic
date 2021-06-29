516c694867702f55b3bcf063d45d08c38b15d108
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-06-29 23:34:39 +0700

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -12,7 +12,7 @@ for n in range(N-1):
     x[n+1] = λ*x[n]*(1-x[n])
     
     
-x = np.around(x*1000)
+x = np.around(x*2500)
     
 fig = go.Figure(data=go.Scatter(y=x , mode='lines+markers'))
 st.plotly_chart(fig)
