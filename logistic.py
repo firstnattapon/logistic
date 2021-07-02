c63bf42331faa53075f8e9f9043265fdae7b2134
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-02 16:42:46 +0700

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -3,7 +3,103 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import plotly.graph_objects as go
 import plotly.express as px
+import pandas as pd
+import pandas_datareader  as pdr
+import ccxt
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+from datetime import datetime
 
+class  delta :
+    def __init__(self , usd = 1000 , fix_value = 0.50, pair_data = 'SRM-PERP', timeframe = '5m' 
+                 , limit  = 2016 , series_num = [None] , minimum_re = 0.001):
+        self.usd    = usd
+        self.fix_value  = fix_value
+        self.pair_data = pair_data
+        self.timeframe = timeframe
+        self.limit = limit
+        self.series_num = series_num
+        self.minimum_re = minimum_re
+
+    def get_data(self):
+        exchange = ccxt.ftx({'apiKey': '', 'secret': '', 'enableRateLimit': True})
+        ohlcv = exchange.fetch_ohlcv(self.pair_data, self.timeframe, limit=self.limit)
+        ohlcv = exchange.convert_ohlcv_to_trading_view(ohlcv)
+        df = pd.DataFrame(ohlcv)
+        df.t = df.t.apply(lambda x: datetime.fromtimestamp(x))
+        df = df.set_index(df['t']);
+        df = df.drop(['t'], axis=1)
+        df = df.rename(columns={"o": "open", "h": "high", "l": "low", "c": "close", "v": "volume"})
+        df = df.drop(['open', 'high' , 'low' , 'volume'] , axis=1) 
+        df = df.dropna()
+        return df
+
+    def series(self):
+        series  = self.get_data()
+        series['index'] = [ i for i in range(len(series))]
+        series['perdit'] =series['index'].apply(func= (lambda x : np.where( x in self.series_num , 1 , 0)))
+        return series
+
+    def  nav (self):
+        nav_data = self.series()
+        nav_data['amount'] =  np.nan
+        for i in range(len(nav_data)): # amount
+            if i == 0 :
+                nav_data.iloc[i, 3] =   (self.usd * self.fix_value)  / nav_data.iloc[i, 0]
+            else :
+                nav_data.iloc[i, 3] =   np.where(nav_data.iloc[i, 2] == 0 ,  nav_data.iloc[i-1, 3] , 
+                                                (self.usd * self.fix_value) / nav_data.iloc[i, 0])          
+                
+        nav_data['asset_value'] =  (nav_data['close']*nav_data['amount']) 
+
+        nav_data['re'] =  np.nan
+        for i in range(len(nav_data)): # re
+            if i == 0 :
+                nav_data.iloc[i, 5] =    0
+            else :
+                nav_data.iloc[i, 5] =    np.where(nav_data.iloc[i, 2] == 1 and 
+                                                  (abs((nav_data.iloc[i-1, 3] * nav_data.iloc[i, 0]) - (self.usd * self.fix_value)) 
+                                                  / (self.usd * self.fix_value)) >= self.minimum_re
+                                                  , (nav_data.iloc[i-1, 3] * nav_data.iloc[i, 0]) -  (self.usd * self.fix_value) , 0)
+
+        nav_data['cash'] =  np.nan
+        for i in range(len(nav_data)): # cash
+            if i == 0 :
+                nav_data.iloc[i, 6] =    (self.usd * self.fix_value)
+            else :
+                nav_data.iloc[i, 6] =   (nav_data.iloc[i-1, 6] + nav_data.iloc[i, 5] )
+
+        nav_data['sumusd'] =  (nav_data['cash'] + nav_data['asset_value'])
+
+        return nav_data
+
+    def  mkt (self):
+        mkt_data  = self.nav()
+        mkt_data[':'] = ':'
+        mkt_data['cash_mkt'] =  (self.usd * self.fix_value)
+        mkt_data['amount_mkt'] =   (mkt_data.iloc[0, 9]  / mkt_data.iloc[0, 0])
+        mkt_data['assetvalue_mkt'] =  mkt_data['close'] * mkt_data['amount_mkt']
+        mkt_data['sumusd_mkt'] = (mkt_data['assetvalue_mkt']  + mkt_data['cash_mkt'])
+
+        return mkt_data
+
+    def cf (self):
+        cf_data = self.mkt()
+        cf_data[': '] = ': '
+        cf_data['cf_usd'] =   cf_data['sumusd']  - cf_data['sumusd_mkt']
+        cf_data['cf_change'] =  (cf_data['cf_usd'] /  cf_data.iloc[0 , 7]) * 100
+
+        return cf_data
+
+    def change (self):
+        change_data = self.cf()
+        change_data[' : '] = ' : '
+        change_data['price_change'] =   ((change_data['sumusd_mkt'] - change_data.iloc[0, 12]) / change_data.iloc[0, 12]) * 100
+        change_data['pv_change']  = ((change_data['sumusd'] - change_data.iloc[0 , 7] ) / change_data.iloc[0 , 7]) *100
+
+        return change_data
+
+    
 λ = st.sidebar.number_input('λ',3.0)
 N = st.sidebar.slider('N', min_value=50 , max_value=100) 
 x = np.zeros(N)
@@ -23,4 +119,3 @@ st.plotly_chart(fig)
 
 code = np.sort(np.unique(x))
 st.code('{} \n\n n = {}'.format(code , len(code)))
-
