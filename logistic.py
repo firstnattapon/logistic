37a701ba82565ae8e6eb05c0a7904af85dd79911
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-02 16:53:35 +0700

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -119,16 +119,18 @@ st.plotly_chart(fig)
 code = np.sort(np.unique(x))
 st.code('{} \n\n n = {}'.format(code , len(code)))
 
+#  ____________________________________________________________________
 
-#  streamlit
 col1, col2 , col3 , col4 , col5   = st.beta_columns(5)
 pair_data = col1.text_input("pair_data", "CRV/USD")
 fix_value = float(col2.text_input("fix_value", "0.5" ))
 invest =  int(col3.text_input("invest" , "1000"))
-timeframe = col4.text_input("timeframe", "1h")
-limit = int(col5.text_input("limit", "2500"))
+timeframe = col4.text_input("timeframe", "5m")
+limit = int(col5.text_input("limit", "{}".format(max))
 
-delta_A = delta(usd = invest , fix_value = fix_value , pair_data = pair_data , timeframe =  timeframe  , limit  = limit)
+delta_A = delta(usd = invest , fix_value = fix_value , pair_data = pair_data , timeframe =  timeframe  ,
+                limit  = limit ,series_num = code , minimum_re = 0.001 )
+      
 delta_A= delta_A.cf()
 
 _ = delta_A[['cf_change' ,'change_mkt' ,'0' ]] ; _.columns = ['1: cf_%', '2: mkt_%' , "3: zero_line"] 
