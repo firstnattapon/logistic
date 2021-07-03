b5dba2909365e8464fb72fa1d6db0f3c1f7d189c
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-03 21:45:43 +0700

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -126,9 +126,9 @@ fig = px.scatter(x=x ,y=x)
 for l in np.sort(np.unique(x)): fig.add_hline(y=l , line_width=1.0)
 st.plotly_chart(fig)
 
-start = st.sidebar.date_input('start' ,datetime.date(2011,1,1))
-st.write(start.tm_yday)
-end = st.sidebar.date_input('end')
+# start = st.sidebar.date_input('start' ,datetime.date(2011,1,1))
+st.write(datetime.date(2011,1,1).tm_yday)
+# end = st.sidebar.date_input('end')
 
 if st.sidebar.checkbox('linear',value=False) :
     code = [ i for i in range(max)]
