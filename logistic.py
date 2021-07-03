fdfc2a5010d0e9ed1d5592ea525558a9756e5c37
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-04 00:15:48 +0700

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -129,7 +129,7 @@ else :
 fig = go.Figure(data=go.Scatter(y= z , mode='lines+markers'))
 st.plotly_chart(fig)
 
-fig = px.scatter(y=z)
+fig = px.scatter(y=code)
 for l in np.sort(np.unique(z)): fig.add_hline(y=l , line_width=1.0)
 st.plotly_chart(fig)
     
