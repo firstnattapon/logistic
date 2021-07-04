45491a411dc842c03e30a44a5fa8439644eb2b32
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-05 00:33:34 +0700

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -117,7 +117,7 @@ cf =  st.sidebar.checkbox('cf',value=False)
 λ = st.sidebar.number_input('λ', min_value=0.0 , max_value=4.0 , value=4.00)
 N = st.sidebar.number_input('N', min_value=50 , max_value=10000 , value=9999) 
 x = np.zeros(N)
-x[0] = st.sidebar.number_input('x0', min_value=0.001, max_value=0.999, value=0.042 , format="{.3f}")
+x[0] = st.sidebar.number_input('x0', min_value=0.001, max_value=0.999, value=0.042 , format="{:,.3f}")
 max = st.sidebar.number_input('max' ,0 , 5000 ,2304)
 
 col1, col2 , col3 , col4 , col5 , col6   = st.beta_columns(6)
