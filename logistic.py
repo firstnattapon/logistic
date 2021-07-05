eba2263d143d89191da16737cc849cf198efecc1
firstnattapon <61854095+firstnattapon@users.noreply.github.com>
2021-07-05 21:09:40 +0700

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -147,7 +147,7 @@ if loop:
     pair_z = df['p'].values
     pair_data = st.sidebar.selectbox('pair_data', pair_z)    
 else :
-    pair_data = st.sidebar.selectbox('pair_data', pair_x , 35)    
+    pair_data = st.sidebar.selectbox('pair_data', pair_x , 30)    
 
 col2 , col3 , col4 , col5 , col6   = st.beta_columns(5)
 fix_value = float(col2.text_input("fix_value", "0.5" ))
